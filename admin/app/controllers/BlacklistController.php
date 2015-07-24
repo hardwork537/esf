3355f269d0d5c56c6b3c8ab4d0d57287feb7abd8
tonyzhao <jihui.zhao@imike.com>
2015-07-24 13:27:18 +0800

    blacklist

--- admin/app/controllers/BlacklistController.php
+++ admin/app/controllers/BlacklistController.php
@@ -82,4 +82,102 @@ class BlacklistController extends ControllerBase
             $this->show('JSON', array('status'=>1, 'info'=>'手机号添加失败'));
         }
     }
+    
+    public function delAction($id = 0)
+    {
+        $id = intval($id);
+        $black = Blacklist::findFirst($id);
+        if(!$black)
+        {
+            $this->show('JSON', array('status'=>1, 'info'=>'不存在'));
+        }
+        
+        $delRes = $black->delete();
+        if($delRes)
+        {
+            $this->show('JSON', array('status'=>0, 'info'=>'删除成功'));
+        } else {
+            $this->show('JSON', array('status'=>1, 'info'=>'删除失败'));
+        }
+    }
+    public function importAction()
+    {
+        $rs = Utility::importCsv();
+        if($rs == 1)
+        {
+            $str = "文件类型不正确";
+            echo "<script>parent.doframe('".$str."')</script>";
+            die();
+        }
+        elseif($rs == 2)
+        {
+            $str = "请选择上传的文件";
+            echo "<script>parent.doframe('".$str."')</script>";
+            die();
+        }
+        elseif(is_array($rs))
+        {
+            set_time_limit(0);
+
+            $str  = "";
+            $data = $label_names = array();
+            foreach($rs as $k => $v)
+            {
+                $n = $k + 2;
+
+                $phone  = trim($v[0]);
+                $remark  = trim($v[1]);
+
+                if(!preg_match('/^1[0-9]{10}$/', $phone))
+                {
+                    $str = "第 {$n} 行手机格式错误！";
+                    break;
+                }
+                
+                $data[$phone] = array(
+                    'phone' => $phone,
+                    'remark'   => $remark ? $remark : 'excel导入'
+                );
+            }
+            
+            if($str == "")
+            {
+                //添加数据
+                $ret = Blacklist::instance()->addBlackList($this->_userInfo['id'], $data);
+                $str = 0 !== $ret['status'] ? "导入失败，请稍后重试！" : "导入成功！";
+            }
+        }
+        else
+        {
+            $str = "未知错误,请重试";
+        }
+
+        echo "<script>parent.doframe('".$str."')</script>";
+        exit();
+    }
+    
+    public function downloadAction()
+    {
+        $filename = ROOT.'src/admin/download/blacklist.csv';
+        if(false == file_exists($filename))
+        {
+            exit($filename);
+            return false;
+        }
+
+        // http headers
+        header('Content-Type: application-x/force-download');
+        header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+        header('Content-length: '.filesize($filename));
+
+        // for IE6
+        if(false === strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6'))
+        {
+            header('Cache-Control: no-cache, must-revalidate');
+        }
+        header('Pragma: no-cache');
+
+        // read file content and output
+        return readfile($filename);;
+    }
 }
--- models/Blacklist.php
+++ models/Blacklist.php
@@ -41,4 +41,53 @@ class Blacklist extends BaseModel
         return parent::_instance(__CLASS__, $cache);
     }
 
+    /**
+     * 批量添加
+     * @param type $data
+     * @return type
+     */
+    public function addBlackList($operatorId, $data = array())
+    {
+        if(!is_array($data) || count($data) == 0)
+        {
+            return array('status'=>1, 'info'=>'数据为空');
+        }
+        $phones = array_keys($data);
+        
+        $conditions = array(
+            'conditions' => "phone in(".  implode(',', $phones).")",
+            'columns' => 'phone'
+        );
+        $res = self::find($conditions, 0)->toArray();
+        foreach($res as $v)
+        {
+            unset($data[$v['phone']]);
+        }
+        if(empty($data))
+        {
+            return array('status'=>0);
+        }
+        $operatorId = intval($operatorId);
+        $addSql = "INSERT INTO ".$this->getSource()."(blOperator,blPhone,remark,blAddTime) VALUES";
+        $i = 0;
+        foreach($data as $v)
+        {
+            if($i > 0)
+            {
+                $addSql .= ',';
+            }
+            $phone = $v['phone'];
+            $remark = $v['remark'];
+            $addTime = date('Y-m-d H:i:s');
+            
+            $addSql .= "({$operatorId},'{$phone}','{$remark}','{$addTime}')";
+        }
+        $addRes = self::instance()->execute($addSql);
+        if($addRes)
+        {
+            return array('status'=>0);
+        } else {
+            return array('status'=>1, 'info'=>'导入失败');
+        }
+    }
 }
