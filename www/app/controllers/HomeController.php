81fa04f1981b3311e05e3e211a0fc28853963418
tonyzhao <jihui.zhao@imike.com>
2015-07-30 21:44:36 +0800

    html style edit

--- www/app/controllers/HomeController.php
+++ www/app/controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends ControllerBase
     public function indexAction()
     {
         $data = array();
-        $data['cssList'] = array('css/index.css');
+        $data['cssList'] = array('css/index.css?v=20150730001');
         //热门搜索
         global $HOT_SEARCH;
         $data['hot'] = $HOT_SEARCH[2];
